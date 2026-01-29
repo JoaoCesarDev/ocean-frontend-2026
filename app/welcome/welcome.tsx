26cceeb6274fe41f99f0a3fe2fcf15e58fdf3b6f
João César Rodrigues Ferreira <66702938+JoaoCesarDev@users.noreply.github.com>
2026-01-29 00:41:51 +0000

    atualização para consumir a api rickandmorty

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,30 +1,48 @@
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
+import { useEffect, useState } from "react";
+import { Personagem } from "./components/personagem";
+import { Titulo } from "./components/titulo";
+
+
 
 export function Welcome() {
+  const [personagens, setPersonagens] = useState<{
+    name: string,
+    image: string,
+    species: string,
+    status: string
+    }[]>([])
+  
+    useEffect(()=>{
+      async function buscarPersonagens(){
+        const resposta = await fetch("https://rickandmortyapi.com/api/character");
+        const resultado = await resposta.json()
+        setPersonagens(resultado.results)
+      }
+      buscarPersonagens()
+    },[])
   return (
-    
     <>
-      <div className="header">
-        <h1>
-          Rick and Morty
-        </h1>
-      </div>
+    <Titulo/>
       <div className="personagens">
-        <div className="personagem">
-          <img 
-            src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" 
-          />
-          <p>
-            Rick Sanchez
-          </p>
-          <p>
-            Human
-          </p>
-          <span>
-            Alive
-          </span>
-        </div>
+        {
+          // Aqui percorremos a lista de personagems
+          personagens.map((
+            personagem
+            // Aqui eu acesso a variavel que corresponde
+            // ao item da lista que estou percorrendo
+          ) => {
+            return (
+              // Retorno o componente do meu personagem
+              // com os dados do personagem
+              <Personagem 
+                nome={personagem.name}
+                imagem={personagem.image}
+                especie={personagem.species}
+                vida={personagem.status}
+              />
+            )
+          })
+        }
       </div>
     </>
   );
